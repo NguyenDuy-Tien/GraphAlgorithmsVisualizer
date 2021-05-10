1633f133c939bbd868e354a1eeefd8f5363b1b20
haitranquang-official <haitranquang.official@gmail.com>
2021-05-10 09:02:05 +0700

    InputMenu sample code added

---
+++ Project/Java Code for Visualization/src/Execution/InputMenuController.java
@@ -0,0 +1,22 @@
+package Execution;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class InputMenuController implements Initializable {
+	@FXML
+	VBox vBox;
+	@FXML
+	AnchorPane anchorPane;
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
