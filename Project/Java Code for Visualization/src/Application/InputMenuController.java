b4085123a0afd8d7f888ff547eca54a67499ccd7
“Tiennd4857” <tien.nd194857@sis.hust.edu.vn>
2021-05-27 00:01:59 +0700

    Draw from input File & 2 way directed edge

--- Project/Java Code for Visualization/src/Application/InputMenuController.java
+++ Project/Java Code for Visualization/src/Application/InputMenuController.java
@@ -1,10 +1,15 @@
 package Application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+
+import com.jfoenix.controls.JFXButton;
 
 import Algorithm.Graph;
 import Elements.DirectedEdge;
@@ -12,6 +17,7 @@ import Elements.Edge;
 import Elements.UndirectedEdge;
 import Elements.Vertex;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -20,9 +26,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.FileChooser;
 
 public class InputMenuController implements Initializable{
 	@FXML
@@ -33,6 +42,10 @@ public class InputMenuController implements Initializable{
 	ComboBox AlgorithmBox;
 	@FXML
 	TextArea graphData;
+	@FXML
+	JFXButton selectFileButton;
+	@FXML
+	Label filePathLabel;
 	public static Graph graph = new Graph();
 	public static boolean undirected = false, directed = false;
 	public static void showAlert(String title, String message)
@@ -42,6 +55,62 @@ public class InputMenuController implements Initializable{
 		alert.setHeaderText(title);
 		alert.setContentText(message);
 		alert.showAndWait();
+	}
+	
+	public void chooseFile(ActionEvent event) {
+		FileChooser fc = new FileChooser();
+		File file = fc.showOpenDialog(null);
+		if(file != null) {
+			filePathLabel.setText(file.getAbsolutePath());
+			getInputFromFile(file);
+		}
+		
+	}
+	void getInput(String graphText) {
+		String[] splitdata = graphText.split("\\s+");
+		int number_of_vertices = Integer.valueOf(splitdata[0]);
+		int number_of_edges = Integer.valueOf(splitdata[1]);
+		if (splitdata.length > (3*number_of_edges + 2))
+		{
+			showAlert("Invalid data.", "(First line has 2 number N,M - number of vertices and number of edges; the next M lines each have a combination u, v, w: begin - end - weight)");
+		}
+		graph = new Graph();
+		for(int i = 0; i < number_of_vertices; i++) {
+			Random random = new Random();
+			Vertex vertex = new Vertex(random.nextDouble() * 500 , random.nextDouble() * 400 - random.nextDouble()*40, 12.0);
+			graph.addVertex(vertex);
+		}
+		for (int i = 0; i < number_of_edges; i++){
+			int u, v;
+			double w;
+			u = Integer.valueOf(splitdata[2 + 3*i]);
+			v = Integer.valueOf(splitdata[3 + 3*i]);
+			w = Integer.valueOf(splitdata[4 + 3*i]);
+			Vertex beginVertex = graph.getList_of_vertices().elementAt(u - 1);
+			Vertex endVertex = graph.getList_of_vertices().elementAt(v - 1);
+			if(undirected) {
+				UndirectedEdge edge = new UndirectedEdge(beginVertex, endVertex, w);
+				graph.addEdge(edge);
+			}
+			else if(directed) {
+				DirectedEdge edge = new DirectedEdge(beginVertex, endVertex, w);
+				graph.addEdge(edge);
+			}
+		}
+	}
+	void getInputFromFile(File inputFile) {
+		try {
+			Scanner reader = new Scanner(inputFile);
+			StringBuilder string = new StringBuilder();
+			while(reader.hasNextLine()) {
+				String dataString = reader.nextLine();
+				string.append(dataString).append("\n");
+			}
+			graphData.setText(string.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public void validate()
 	{
@@ -79,36 +148,7 @@ public class InputMenuController implements Initializable{
 			String graphText = graphData.getText().trim();
 			if (!graphText.equals(""))
 			{
-				String[] splitdata = graphText.split("\\s+");
-				int number_of_vertices = Integer.valueOf(splitdata[0]);
-				int number_of_edges = Integer.valueOf(splitdata[1]);
-				if (splitdata.length > (3*number_of_edges + 2))
-				{
-					showAlert("Invalid data.", "(First line has 2 number N,M - number of vertices and number of edges; the next M lines each have a combination u, v, w: begin - end - weight)");
-				}
-				graph = new Graph();
-				for(int i = 0; i < number_of_vertices; i++) {
-					Random random = new Random();
-					Vertex vertex = new Vertex(random.nextDouble() * 500 , random.nextDouble() * 400 - random.nextDouble()*40, 12.0);
-					graph.addVertex(vertex);
-				}
-				for (int i = 0; i < number_of_edges; i++){
-					int u, v;
-					double w;
-					u = Integer.valueOf(splitdata[2 + 3*i]);
-					v = Integer.valueOf(splitdata[3 + 3*i]);
-					w = Integer.valueOf(splitdata[4 + 3*i]);
-					Vertex beginVertex = graph.getList_of_vertices().elementAt(u - 1);
-					Vertex endVertex = graph.getList_of_vertices().elementAt(v - 1);
-					if(undirected) {
-						UndirectedEdge edge = new UndirectedEdge(beginVertex, endVertex, w);
-						graph.addEdge(edge);
-					}
-					else if(directed) {
-						DirectedEdge edge = new DirectedEdge(beginVertex, endVertex, w);
-						graph.addEdge(edge);
-					}
-				}
+				getInput(graphText);
 				
 			}
 			System.out.println("GraphText: " + graphText + valid);
